8270f40a6dab1f1bab93de8729188965aeeda023
gray <gray.b@kakaocorp.com>
2023-04-20 19:47:51 +0900

    modify package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SubApp2",
-            dependencies: []),
+            dependencies: [.product(name: "Lottie", package: "Lottie")]),
         .testTarget(
             name: "SubApp2Tests",
             dependencies: ["SubApp2"]),
